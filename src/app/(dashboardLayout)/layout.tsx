d67e7b2996dd835f0533a837c932eca93244d0ed
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-01 01:21:02 +0600

    feat: Generating layout

---
+++ src/app/(dashboardLayout)/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Apollo Gears",
+  description: "Welcome to Apollo Gears: The Ultimate Ride Sharing Platform",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <body className={`antialiased`}>{children}</body>;
+}
