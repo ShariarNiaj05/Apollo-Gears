adb61a8ad555012c93c06ff46421c8d95804ca78
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-01 01:25:00 +0600

    feat: Generating layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
-        Common Navbar
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
