4ea64065fe7f913f5452bc19738cc8a497e255a0
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-02 01:12:43 +0600

    ui: creating necessary page

--- src/app/(dashboardLayout)/(userDashboard)/dashboard/my-requested-rides/page.tsx
+++ src/app/(dashboardLayout)/(userDashboard)/dashboard/my-requested-rides/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-const page = () => {
-  return <div></div>;
+const MyRequestedRides = () => {
+  return <div>MyRequestedRides</div>;
 };
 
-export default page;
+export default MyRequestedRides;
