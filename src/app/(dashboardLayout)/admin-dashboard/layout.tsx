029662b526e90b28b2a7173736c4774a9c7d53ec
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-02 01:03:04 +0600

    ui: creating necessary page

---
+++ src/app/(dashboardLayout)/admin-dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Apollo Gears Dashboard",
+  description: "Welcome to Apollo Gears: The Ultimate Ride Sharing Platform",
+};
+
+export default function AdminDashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      Dashboard Common Navbar
+      {children}
+    </div>
+  );
+}
