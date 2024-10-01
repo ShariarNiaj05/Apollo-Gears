d289bc2044ee5b455be32dc1cb7258d4891d9906
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-01 23:42:09 +0600

    ui: creating necessary page

---
+++ src/app/(commonLayout)/cars/[id]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useParams } from "next/navigation";
+
+const CarDetailsPage = () => {
+  const { id } = useParams();
+  return <div>CarDetailsPage{id}</div>;
+};
+
+export default CarDetailsPage;
