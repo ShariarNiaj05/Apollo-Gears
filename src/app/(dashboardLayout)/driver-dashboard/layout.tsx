3c96c39c45f28b2571c12594332534a6001c9d2f
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-02 01:02:25 +0600

    ui: creating necessary page

---
+++ src/app/(dashboardLayout)/driver-dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Apollo Gears Dashboard",
+  description: "Welcome to Apollo Gears: The Ultimate Ride Sharing Platform",
+};
+
+export default function DriverDashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div>{children}</div>;
+}
--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   description: "Welcome to Apollo Gears: The Ultimate Ride Sharing Platform",
 };
 
-export default function DashboardLayout({
+export default function CommonDashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
