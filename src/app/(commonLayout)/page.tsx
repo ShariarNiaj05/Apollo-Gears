2d80151c036ccd8d86f46ae79124c66e5fbee881
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-04 01:31:54 +0600

    style: setting up styling component

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@nextui-org/react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <Button>Press me</Button>
+    </div>
+  );
 };
 
 export default HomePage;
