20e824e2b11b55d33aaa77c5dd5789dedae5c975
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-04 01:26:43 +0600

    style: setting up styling component

--- src/app/lib/providers.tsx
+++ src/app/lib/providers.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
 
-function Providers({ children }) {
+function Providers({ children }: { children: React.ReactNode }) {
   // 2. Wrap NextUIProvider at the root of your app
   return <NextUIProvider>{children}</NextUIProvider>;
 }
