d3c39f778602d2ef8296c7683e7be7aa65c9bd93
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-05 00:05:14 +0600

    auth: implementing authentication system

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,10 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
 });
 
 export { handler as GET, handler as POST };
