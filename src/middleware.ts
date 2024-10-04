c1128e8f22302d59db0ab87da0fbf3de1c54ae37
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-05 01:10:50 +0600

    auth: implementing authorization system

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
-export async function middleware() {}
+import { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {}
 
 export const config = {
   matcher: [
+    "/login",
+    "/register",
     "/dashboard/:page*",
     "/admin-dashboard/:page*",
     "driver-dashboard/:page*",
