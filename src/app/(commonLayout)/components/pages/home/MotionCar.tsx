75ad3abfd94692e9b1b0ffa5e7d2c92fb7fe65a9
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-04 15:43:31 +0600

    ui: banner section

--- src/app/(commonLayout)/components/pages/home/MotionCar.tsx
+++ src/app/(commonLayout)/components/pages/home/MotionCar.tsx
@@ -12,7 +12,7 @@ export default function MotionCar() {
       <Image
         alt="Card background"
         className="object-cover rounded-xl"
-        src="../../assets/hero-car.svg"
+        src="../../../../../../public/assets/hero-icon.svg"
       />
     </motion.div>
   );
