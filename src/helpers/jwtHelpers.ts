4fa8bbe205ca2912bedc597c05270a2424d7f048
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-05 01:44:11 +0600

    auth: jwt token verfication

--- src/helpers/jwtHelpers.ts
+++ src/helpers/jwtHelpers.ts
@@ -1,3 +1,19 @@
-export const jwtVerify = (token: string) => {};
+import { JwtPayload } from "jsonwebtoken";
+
+export const jwtVerify = (token: string) => {
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_ACCESS_SECRET as string
+    ) as JwtPayload;
+
+    console.log(decoded, "decoded");
+
+    return decoded;
+  } catch (error: any) {
+    console.log(error);
+    return null;
+  }
+};
 
 export const decode = (token: string) => {};
