be416f99a2e07f292ec8409ac0de447f12daf3a8
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-04 15:59:52 +0600

    ui: navbar section

--- src/app/lib/providers.tsx
+++ src/app/lib/providers.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
-
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 function Providers({ children }: { children: React.ReactNode }) {
   // 2. Wrap NextUIProvider at the root of your app
   return (
