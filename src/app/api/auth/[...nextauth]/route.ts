3e718961f82ec7d0241475f3a8d8e9922bb55f06
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-05 00:54:11 +0600

    auth: implementing authentication system

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { AuthOptions } from "@/config/nextauth.config";
 import NextAuth from "next-auth";
 
-const handler = NextAuth();
+const handler = NextAuth(AuthOptions);
 
 export { handler as GET, handler as POST };
