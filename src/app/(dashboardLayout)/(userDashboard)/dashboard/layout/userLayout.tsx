36fd8db5cfa455b0c8c743f547e19194b99cbc59
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-06 00:17:37 +0600

    ui: user dashboard layout and customization

--- src/app/(dashboardLayout)/(userDashboard)/dashboard/layout/userLayout.tsx
+++ src/app/(dashboardLayout)/(userDashboard)/dashboard/layout/userLayout.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { SidebarWrapper } from "@/app/(dashboardLayout)/components/sidebar/userSidebar";
+
 interface Props {
   children: React.ReactNode;
 }
 
 export const UserLayout = ({ children }: Props) => {
-  return <section className="flex">{children}</section>;
+  return (
+    <section className="flex">
+      <SidebarWrapper />
+      {children}
+    </section>
+  );
 };
